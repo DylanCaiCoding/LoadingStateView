8b9f530813c07f027b5d0f135721d8564d876164
Dylanc <caishenglang@foxmail.com>
2021-08-23 20:02:42 +0800

    Optimize BaseActivity sample

--- app/src/main/java/com/dylanc/loadinghelper/sample/base/BaseActivity.java
+++ app/src/main/java/com/dylanc/loadinghelper/sample/base/BaseActivity.java
@@ -16,13 +16,10 @@
 
 package com.dylanc.loadinghelper.sample.base;
 
-import android.view.ViewGroup;
-
 import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dylanc.loadinghelper.LoadingHelper;
-import com.dylanc.loadinghelper.ViewType;
 import com.dylanc.loadinghelper.sample.adapter.NavIconType;
 import com.dylanc.loadinghelper.sample.adapter.ToolbarAdapter;
 
@@ -30,8 +27,8 @@ import com.dylanc.loadinghelper.sample.adapter.ToolbarAdapter;
  * 这是耦合度较低的封装方式，没有任何抽象方法，可以很方便地将基类里的代码拷贝到其它项目的基类里使用。
  *
  * 使用该基类时注意以下事项：
- * 显示对应视图之前需要注册适配器，可以设置全局适配器，某个页面想修改样式时再注册个新的适配器。
- * 设置标题栏的方法应该根据项目需要进行编写，下面提供了参考示例。
+ * 1. 显示对应视图之前需要注册适配器，可以设置全局适配器，某个页面想修改样式时再注册个新的适配器。
+ * 2. 设置标题栏的方法应该根据项目需要进行编写，下面提供了参考示例。
  *
  * @author Dylan Cai
  */
@@ -42,29 +39,32 @@ public class BaseActivity extends AppCompatActivity {
 
   @Override
   public void setContentView(int layoutResID) {
-    this.setContentView(layoutResID, android.R.id.content);
+    this.setContentView(layoutResID, 0);
   }
 
   public void setContentView(int layoutResID, @IdRes int contentViewId) {
-    this.setContentView(layoutResID, contentViewId, null);
+    super.setContentView(layoutResID);
+    if (contentViewId == 0) {
+      loadingHelper = new LoadingHelper(this);
+    } else {
+      loadingHelper = new LoadingHelper(findViewById(contentViewId));
+    }
+    loadingHelper.setOnReloadListener(this::onReload);
   }
 
-  public void setContentView(int layoutResID, @IdRes int contentViewId, LoadingHelper.ContentAdapter<?> contentAdapter) {
-    super.setContentView(layoutResID);
-    loadingHelper = new LoadingHelper(((ViewGroup) findViewById(contentViewId)).getChildAt(0), contentAdapter);
-    loadingHelper.setOnReloadListener(this::onReload);
+  /**
+   * 添加标题栏的示例方法，请根据自己的需求进行修改
+   */
+  public void setToolbar(String title) {
+    setToolbar(title, NavIconType.BACK, 0);
   }
 
   public void setToolbar(String title, NavIconType type) {
     setToolbar(title, type, 0);
   }
 
-  /**
-   * 添加标题栏的示例方法，请根据自己的需求进行修改
-   */
   public void setToolbar(String title, NavIconType type, int menuId) {
-    loadingHelper.register(ViewType.TITLE, new ToolbarAdapter(title, type, menuId, this::onOptionsItemSelected));
-    loadingHelper.setDecorHeader(ViewType.TITLE);
+    loadingHelper.setDecorHeader(new ToolbarAdapter(title, type, menuId, this::onOptionsItemSelected));
   }
 
   public void showLoadingView() {
