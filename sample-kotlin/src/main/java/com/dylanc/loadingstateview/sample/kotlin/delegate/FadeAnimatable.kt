3825c84c61a6a9f181c88a4335b139e75ff82171
DylanCai <caishenglang@foxmail.com>
2025-03-07 23:27:22 +0800

    refactor: optimize the sample of fade animatable

--- sample-kotlin/src/main/java/com/dylanc/loadingstateview/sample/kotlin/delegate/FadeAnimatable.kt
+++ sample-kotlin/src/main/java/com/dylanc/loadingstateview/sample/kotlin/delegate/FadeAnimatable.kt
@@ -7,25 +7,19 @@ import com.dylanc.loadingstateview.ViewType
 /**
  * @author Dylan Cai
  */
-class FadeAnimatable @JvmOverloads constructor(
-  private val duration: Long = DEFAULT_DURATION
-) : LoadingStateView.Animatable {
+class FadeAnimatable(private val duration: Long = 600) : LoadingStateView.Animatable {
   override fun toggleViewsAnimation(showView: View, hideView: View, showViewType: Any, hideViewType: Any) {
-    showView.alpha = 0f
-    showView.visibility = View.VISIBLE
-    showView.animate().alpha(1f).setDuration(duration)
-
     if (showViewType === ViewType.LOADING && hideViewType === ViewType.CONTENT) {
+      showView.visibility = View.VISIBLE
       hideView.visibility = View.GONE
     } else {
+      showView.alpha = 0f
+      showView.visibility = View.VISIBLE
+      showView.animate().alpha(1f).setDuration(duration)
       hideView.animate().alpha(0f).setDuration(duration).withEndAction {
         hideView.alpha = 1f
         hideView.visibility = View.GONE
       }
     }
-  }
-
-  companion object {
-    private const val DEFAULT_DURATION: Long = 600
   }
 }
